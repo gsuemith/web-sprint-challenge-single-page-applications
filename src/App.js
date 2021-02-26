e3d8ee0bfb764487e16adf9bfff627910ba4c37d
Garrick Suemith <garrick.suemith@gmail.com>
2021-02-26 16:39:04 -0500

    form submission working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router,
   Route, Switch } from 'react-router-dom'
+import axios from 'axios'
 import Header from './components/Header'
 import Home from './routes/Home'
 import Pizza from './routes/Pizza'
 import './App.css'
 
+const initialFormValues = {
+  name: '',
+  size: '',
+  //toppings
+  pepperoni: false,
+  sausage: false,
+  mushroom: false,
+  olive: false,
+  greenPepper: false,
+  bacon: false,
+  chicken: false,
+  anchovy: false,
+  //Quantity
+  quantity: 1,
+  //special instructions
+  instructions: '',
+}
+
+const toppingsList = ['pepperoni', 'sausage', 'mushroom',
+  'olive', 'greenPepper', 'bacon', 'chicken', 'anchovy']
+
 const App = () => {
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [order, setOrder] = useState([])
+
+  //post order
+  const postOrder = (orderItem) => {
+    axios.post('https://reqres.in/api/users')
+  }
+
+  //update values
+  const updateValues = (name, value) => {
+    setFormValues({...formValues, [name]:value})
+  }
+
+  //submit form
+  const formSubmit = () => {
+    const newOrderItem = {
+      name: formValues.name,
+      size: formValues.size,
+      toppings: toppingsList
+        .filter(topping => formValues[topping]),
+      instructions: formValues.instructions,
+      quantity: formValues.quantity,
+    }
+
+    console.log(newOrderItem)
+  }
+
+  
   return (
     <Router>
       <Header/>
       <Switch>
-        
+
         <Route path='/pizza'>
-          <Pizza/>
+          <Pizza values={formValues} 
+            update={updateValues}
+            submit={formSubmit}
+          />
         </Route>
 
         <Route path='/' component={Home}/>
--- src/routes/Pizza.js
+++ src/routes/Pizza.js
@@ -1,12 +1,145 @@
 import React from 'react'
 import '../App.css'
 
-const Pizza = () => {
+const Pizza = ({ values, update, submit }) => {
+
+	const onChange = evt => {
+		const { name, value, type, checked } = evt.target
+		update(name, type === 'checkbox' ? checked : value)
+	}
+
+	const onSubmit = evt => {
+		evt.preventDefault();
+		submit()
+	}
+
 	return (
 		<div className="order-form">
 			<div className="order-card">
 				<h4>Build Your Own Pizza</h4>
 				<div className="banner"></div>
+
+				{/* Options begin here */}
+				<form className="options" onSubmit={onSubmit}>
+					<label>
+						<h5>Name</h5>
+						<input type="text"
+							onChange={onChange}
+							name="name"
+						/>
+					</label>
+
+					<h5>Choice of Size</h5>
+					<select name="size" value={values.size} onChange={onChange}>
+						<option value="">---Select a Size---</option>
+						<option value="S">Small 10 in.</option>
+						<option value="M">Medium 12 in.</option>
+						<option value="L">Large 14 in.</option>
+						<option value="XL">Extra-Large 16 in.</option>
+					</select>
+
+					<h5>Add Toppings</h5>
+					<div className="toppings">
+						<label>
+							<input type="checkbox"
+								onChange={onChange}
+								name="pepperoni"
+								checked={values.pepperoni}
+								value={"pepperoni"}
+							/>
+							Pepperoni
+						</label>
+						<label>
+							<input type="checkbox"
+								onChange={onChange}
+								name="sausage"
+								checked={values.sausage}
+								value={"sausage"}
+							/>
+							Italian Sausage
+						</label>
+						<label>
+							<input type="checkbox"
+								onChange={onChange}
+								name="mushroom"
+								checked={values.mushroom}
+								value={"mushroom"}
+							/>
+							Mushrooms
+						</label>
+						<label>
+							<input type="checkbox"
+								onChange={onChange}
+								name="olive"
+								checked={values.olive}
+								value={"olive"}
+							/>
+							Black Olives
+						</label>
+						<label>
+							<input type="checkbox"
+								onChange={onChange}
+								name="greenPepper"
+								checked={values.greenPepper}
+								value={"greenPepper"}
+							/>
+							Green Peppers
+						</label>
+						<label>
+							<input type="checkbox"
+								onChange={onChange}
+								name="bacon"
+								checked={values.bacon}
+								value={"bacon"}
+							/>
+							Bacon
+						</label>
+						<label>
+							<input type="checkbox"
+								onChange={onChange}
+								name="chicken"
+								checked={values.chicken}
+								value={"chicken"}
+							/>
+							Grilled Chicken
+						</label>
+						<label>
+							<input type="checkbox"
+								onChange={onChange}
+								name="anchovy"
+								checked={values.anchovy}
+								value={"anchovy"}
+							/>
+							Anchovies
+						</label>
+					</div>
+
+					<div className="special-instructions">
+						<h5>Special Instructions</h5>
+						<input type="text"
+							name="instructions"
+							onChange={onChange}
+							value={values.instructions}
+							maxLength="50"
+						/>
+					</div>
+
+					<div className="submit-order">
+						<label>
+							How many?
+							<input type="number"
+							min="1" max="10"
+							value={values.quantity}
+							onChange={onChange}
+							name="quantity"
+						/>
+						</label>
+						
+						<button>Add to Order</button>
+					</div>
+				</form>
+				
+
 			</div>
 		</div>
 	)
